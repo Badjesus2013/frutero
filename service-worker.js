167e74da0d94f979cb9e1c9559e9b9a2e00e1e62
Jesus Castañeda <106368235+Badjesus2013@users.noreply.github.com>
2026-09-19 21:49:53 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE = 'frutero-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copia = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copia));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
