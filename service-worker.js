3468bb983cf2c83dee9935ff02d6f195a78e5a36
Jesus Castañeda <106368235+Badjesus2013@users.noreply.github.com>
2026-09-25 11:04:46 -0600

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='frutero-v2-6-borrar-offline';
+const CACHE='frutero-v2-7-venta-bolsa';
 const SHELL=['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL.map(url=>new Request(url,{cache:'reload'})))));
